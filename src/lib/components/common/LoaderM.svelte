1b78aca54077fd03ddf01522d5c9d2e6bfba7dc7
bojdavid <bojdavid80@gmail.com>
2025-11-13 16:14:40 +0100

    changed spotify logo dir

--- src/lib/components/common/LoaderM.svelte
+++ src/lib/components/common/LoaderM.svelte
@@ -5,5 +5,5 @@
   <span
     class="absolute inline-flex h-full w-full rounded-full bg-spotify-black opacity-50 animate-ping"
   ></span>
-  <img src="/spotify_logo.png" alt="spotify logo" class="h-full w-auto" />
+  <img src="spotify_logo.png" alt="spotify logo" class="h-full w-auto" />
 </div>
