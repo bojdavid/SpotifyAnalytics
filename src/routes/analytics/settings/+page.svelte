06e721ee78e7d4a53b0948077bc82918c603d26e
bojdavid <bojdavid80@gmail.com>
2025-11-13 16:48:13 +0100

    used component loader in settings

--- src/routes/analytics/settings/+page.svelte
+++ src/routes/analytics/settings/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-  import { Loader } from "@lucide/svelte";
-  //import Spotify_Logo from "../../../../static/Spotify_logo.png";
+  import LoaderM from "$lib/components/common/LoaderM.svelte";
 </script>
 
 <section
   class="h-screen w-full bg-spotify-green flex justify-center items-center"
 >
-  <!--
-    <div class=" animate-spin">
-      <Loader size={60} />
-    </div>
--->
-  <div class=" h-40 relative">
-    <span
-      class="absolute inline-flex h-full w-full rounded-full bg-spotify-black opacity-50 animate-ping"
-    ></span>
-    <img src="/spotify_logo.png" alt="spotify logo" class="h-full w-auto" />
-  </div>
+  <LoaderM />
 </section>
