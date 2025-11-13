cef49ea0af31bd2ee7c8a9cb05ca7bdff04502d3
bojdavid <bojdavid80@gmail.com>
2025-11-13 16:20:07 +0100

    corrected spotifyimage path

--- src/lib/components/common/LoaderM.svelte
+++ src/lib/components/common/LoaderM.svelte
@@ -5,5 +5,9 @@
   <span
     class="absolute inline-flex h-full w-full rounded-full bg-spotify-black opacity-50 animate-ping"
   ></span>
-  <img src="spotify_logo.png" alt="spotify logo" class="h-full w-auto" />
+  <img
+    src="%sveltekit.assets%/spotify_logo.png"
+    alt="spotify logo"
+    class="h-full w-auto"
+  />
 </div>
