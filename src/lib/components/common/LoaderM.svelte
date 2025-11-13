39b69138b401df15e7fc026fb60945da8d88d252
bojdavid <bojdavid80@gmail.com>
2025-11-13 16:43:34 +0100

    used ai to optimize the code to see if it will correct the mobile glitch

--- src/lib/components/common/LoaderM.svelte
+++ src/lib/components/common/LoaderM.svelte
@@ -1,13 +1,10 @@
 <script>
+  import spotifyLogo from "../../assets/spotify_logo.png";
 </script>
 
 <div class=" h-40 relative">
   <span
     class="absolute inline-flex h-full w-full rounded-full bg-spotify-black opacity-50 animate-ping"
   ></span>
-  <img
-    src="%sveltekit.assets%/spotify_logo.png"
-    alt="spotify logo"
-    class="h-full w-auto"
-  />
+  <img src={spotifyLogo} alt="spotify logo" class="h-full w-auto" />
 </div>
